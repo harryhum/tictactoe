5be825bc7413024916a257bb6558679acb85028e
Harry Hum <harryhum22@hotmail.com>
2017-12-08 16:57:17 -0500

    Main game features in working order

--- game.rb
+++ game.rb
@@ -1,3 +1,9 @@
+#To do: 
+    #Prompt players for names instead of setting
+    #Separate grid into subclass?
+    #Separate methods into smaller chunks
+    #Make methods private
+
 class Game
     attr_accessor :player
     attr_reader :grid
@@ -17,12 +23,13 @@ class Game
     end
 
     def start
+        @grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
         rand_player
-        starting_player = @player_one.turn == true ? @player_one : @player_two
-        second_player = @player_one.turn == false ? @player_one : @player_two
-        starting_player.sign = 'x' 
-        second_player.sign = 'o'
-        puts "#{starting_player.name} is X and #{second_player.name} is O.\n#{starting_player.name} goes first."
+        @player_x = @player_one.turn == true ? @player_one : @player_two
+        @player_o = @player_one.turn == false ? @player_one : @player_two
+        @player_x.sign = 'x' 
+        @player_o.sign = 'o'
+        puts "#{@player_x.name} is X and #{@player_o.name} is O.\n#{@player_x.name} goes first."
         make_move
     end
 
@@ -45,18 +52,33 @@ class Game
         puts "#{@current_player.name}, make your move"
         get_coordinates
     end
-    
+
     def get_coordinates
         puts "Enter row number:"
         row_num = gets.chomp.to_i
+        while not row_num.between?(0, 2)
+            puts "Please enter a valid row number!"
+            row_num = gets.chomp.to_i
+        end
         puts "Enter column number:"
         column_num = gets.chomp.to_i
+        while not column_num.between?(0, 2)
+            puts "Please enter a valid column number!"
+            column_num = gets.chomp.to_i
+        end
         write_in_grid(row_num, column_num)
     end
 
     def write_in_grid(row, column)
-        @grid[row][column] = @current_player.sign
+        if cell_blank?(row, column)
+            @grid[row][column] = @current_player.sign
+        else
+            display_grid
+            puts "#{@current_player.name}, the square is already occupied! Please enter the coordinates of a blank square."
+            get_coordinates
+        end
         display_grid
+        win?(row, column)
         switch_turns
         make_move
     end
@@ -65,6 +87,52 @@ class Game
         @player_one.turn, @player_two.turn = @player_two.turn, @player_one.turn
     end
 
+    def cell_blank?(row, column)
+        @grid[row][column] == ' ' ? true : false
+    end
+
+    def win?(row, column)
+        if three_in_a_row?(row) || three_in_a_column?(column) || three_in_a_diagonal?
+            puts "#{@grid[row][column] == @player_one.sign ? @player_one.name : @player_two.name} wins! Game over, play again? (Enter y/n)"
+            play_again = gets.chomp
+            play_again == 'y' ? start : exit
+        end
+    end
+
+    def three_in_a_row?(row_num)
+        if cell_blank?(row_num, 0) || cell_blank?(row_num, 1) || cell_blank?(row_num, 2)
+            false
+        else
+            if @grid[row_num][0] == @grid[row_num][1] && @grid[row_num][0] == @grid[row_num][2]
+                true
+            else
+                false
+            end
+        end
+    end
+
+    def three_in_a_column?(column_num)
+        if cell_blank?(0, column_num) || cell_blank?(1, column_num) || cell_blank?(2, column_num)
+            false
+        else
+            if @grid[0][column_num] == @grid[1][column_num] && @grid[0][column_num] == @grid[2][column_num]
+                true
+            else
+                false
+            end
+        end
+    end
+
+    def three_in_a_diagonal?
+        case
+        when @grid[0][0] == @grid[1][1] && @grid[0][0] == @grid[2][2]
+            cell_blank?(0, 0) || cell_blank?(1, 1) || cell_blank?(2, 2) ? false : true
+        when @grid[2][0] == @grid[1][1] && @grid[2][0] == @grid[0][2]
+            cell_blank?(0, 0) || cell_blank?(1, 1) || cell_blank?(2, 2) ? false : true
+        else
+          false
+        end
+    end    
 end
 
 #New game with player_one and player_two
