6fda0cef8ea5acd7580000557c45114867fdb22d
Harry Hum <harryhum22@hotmail.com>
2017-12-15 12:24:38 -0500

    Cleaned and reorganized code

--- game.rb
+++ game.rb
@@ -1,145 +1,141 @@
 class Game
+    attr_reader :win, :tie
+
     def initialize
-        name_entry
+        @player_one = Player.new(1)
+        @player_two = Player.new(2)
+        @current_player = nil
+    end
+
+    def win
+        @win
+    end
+
+    def tie
+        @tie
     end
 
     class Player
-        attr_accessor :name, :sign, :turn
-
-        def initialize(name)
-            @name = name
+        attr_accessor :name, :sign
+        
+        def initialize(player_number)
+            puts "Player #{player_number}, enter your name:"
+            @name = gets.chomp
         end
     end
 
-    private 
-
-    def name_entry
-        puts "Player 1 enter your name:"
-        player_one_name = gets.chomp
-        puts "Player 2 enter your name:"
-        player_two_name = gets.chomp
-        @player_one = Player.new(player_one_name)
-        @player_two = Player.new(player_two_name)
-        start
-    end
-
-    def start
+    def set
+        @board = {a1: " ", a2: " ", a3: " ",
+        b1: " ", b2: " ", b3: " ",
+        c1: " ", c2: " ", c3: " "}
         @moves = 0
-        @grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-        rand_player
-        @player_x = @player_one.turn == true ? @player_one : @player_two
-        @player_o = @player_one.turn == false ? @player_one : @player_two
-        @player_x.sign = 'x' 
-        @player_o.sign = 'o'
-        puts "#{@player_x.name} is X and #{@player_o.name} is O.\n#{@player_x.name} goes first."
-        make_move
+        @win = false
+        @tie = false
     end
 
-    def rand_player
-        @player_one.turn = rand > 0.5 ? true : false
-        @player_two.turn = @player_one.turn == false ? true : false
+    def assign_signs
+        @players = [@player_one, @player_two]
+        signs = rand < 0.5 ? ['x', 'o'] : ['o', 'x']
+        @player_one.sign, @player_two.sign = signs[0], signs[1]
+        puts "#{@player_one.name} is #{@player_one.sign.upcase} and #{@player_two.name} is #{@player_two.sign.upcase}."
     end
 
-    def make_move
-        @moves += 1
-        display_grid
-        @current_player = @player_one.turn == true ? @player_one : @player_two
-        puts "#{@current_player.name}, make your move"
-        get_coordinates
+    def who_goes_first?
+        puts 'Simulated rock, paper, scissors to see who goes first...'
+        @current_player = @players[rand.round]
+        sleep(2)
+        puts "#{@current_player.name} goes first."
     end
 
-    def display_grid
-        puts "Grid:"
-        puts "    0    1    2"
-        puts "0 #{@grid[0].inspect}"
-        puts "1 #{@grid[1].inspect}"
-        puts "2 #{@grid[2].inspect}"
+    def display
+        puts "Board:"
+        puts "    1   2   3"
+        puts "a  [#{@board[:a1]}] [#{@board[:a2]}] [#{@board[:a3]}]"
+        puts "b  [#{@board[:b1]}] [#{@board[:b2]}] [#{@board[:b3]}]"
+        puts "c  [#{@board[:c1]}] [#{@board[:c2]}] [#{@board[:c3]}]"
     end
 
     def get_coordinates
-        puts "Enter row number:"
-        row_num = gets.chomp.to_i
-        while not row_num.between?(0, 2)
-            puts "Please enter a valid row number!"
-            row_num = gets.chomp.to_i
+        puts "Enter row letter:"
+        row_letter = gets.chomp
+        while not row_letter.between?('a', 'c') && row_letter.length == 1
+            puts "Please enter a valid row letter!"
+            row_letter = gets.chomp
         end
         puts "Enter column number:"
         column_num = gets.chomp.to_i
-        while not column_num.between?(0, 2)
+        while not column_num.between?(1, 3)
             puts "Please enter a valid column number!"
             column_num = gets.chomp.to_i
         end
-        write_in_grid(row_num, column_num)
+        @coordinates = row_letter + column_num.to_s
     end
 
-    def write_in_grid(row, column)
-        if cell_blank?(row, column)
-            @grid[row][column] = @current_player.sign
-        else
-            display_grid
-            puts "#{@current_player.name}, the square is already occupied! Please enter the coordinates of a blank square."
+    def write_in_board
+        if @board[@coordinates.to_sym] == ' '
+            @moves += 1
+            @board[@coordinates.to_sym] = @current_player.sign
+        else 
+            puts 'That cell has already been taken!'
             get_coordinates
+            write_in_board
         end
-        display_grid
-        game_over?(row, column)
-        switch_turns
-        make_move
     end
 
     def switch_turns
-        @player_one.turn, @player_two.turn = @player_two.turn, @player_one.turn
+        @current_player = @current_player == @player_one ? @player_two : @player_one
     end
 
-    def cell_blank?(row, column)
-        @grid[row][column] == ' ' ? true : false
-    end
+    def win?
+        win_x = ['x', 'x', 'x']
+        win_o = ['o', 'o', 'o']
 
-    def game_over?(row, column)
-        if three_in_a_row?(row) || three_in_a_column?(column) || three_in_a_diagonal?
-            puts "#{@grid[row][column] == @player_one.sign ? @player_one.name : @player_two.name} wins! Game over, play again? (Enter y/n)"
-            play_again = gets.chomp
-            play_again == 'y' ? start : exit
-        elsif @moves >= 9
-            puts "It's a tie! Game over, play again? (Enter y/n)"
-            play_again = gets.chomp
-            play_again == 'y' ? start : exit
-        end
-    end
+        winning_combos = 
+            [[@board[:a1], @board[:a2], @board[:a3]],
+             [@board[:b1], @board[:b2], @board[:b3]],
+             [@board[:c1], @board[:c2], @board[:c3]],
+             [@board[:a1], @board[:b2], @board[:c3]],
+             [@board[:a3], @board[:b2], @board[:c1]],
+             [@board[:a1], @board[:b1], @board[:c1]],
+             [@board[:a2], @board[:b2], @board[:c2]],
+             [@board[:a3], @board[:b3], @board[:c3]],]
 
-    def three_in_a_row?(row_num)
-        if cell_blank?(row_num, 0) || cell_blank?(row_num, 1) || cell_blank?(row_num, 2)
-            false
-        else
-            if @grid[row_num][0] == @grid[row_num][1] && @grid[row_num][0] == @grid[row_num][2]
-                true
-            else
-                false
+        winning_combos.each do |combo|
+            if combo == win_x || combo == win_o
+                @win = true
+                puts "#{@current_player.name} wins!"
             end
-        end
+        end 
     end
 
-    def three_in_a_column?(column_num)
-        if cell_blank?(0, column_num) || cell_blank?(1, column_num) || cell_blank?(2, column_num)
-            false
-        else
-            if @grid[0][column_num] == @grid[1][column_num] && @grid[0][column_num] == @grid[2][column_num]
-                true
-            else
-                false
-            end
+    def tie?
+        @tie = @moves >= 9 && @win == false ? true : false
+        if @tie
+            puts "It's a tie!"
         end
     end
-
-    def three_in_a_diagonal?
-        case
-        when @grid[0][0] == @grid[1][1] && @grid[0][0] == @grid[2][2]
-            cell_blank?(0, 0) || cell_blank?(1, 1) || cell_blank?(2, 2) ? false : true
-        when @grid[2][0] == @grid[1][1] && @grid[2][0] == @grid[0][2]
-            cell_blank?(0, 0) || cell_blank?(1, 1) || cell_blank?(2, 2) ? false : true
-        else
-          false
-        end
-    end    
 end
 
-Game.new
+game = Game.new
+
+while true do
+    game.set
+    game.assign_signs
+    game.who_goes_first?
+    game.display
+
+    until game.win || game.tie do
+        game.get_coordinates
+        game.write_in_board
+        game.display
+        game.win?
+        game.tie?
+        game.switch_turns
+    end
+
+    puts "Play again? (y/n)"
+    play_again = gets.chomp
+    if play_again == 'n'
+        exit
+    end
+end
